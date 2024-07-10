ae62151a4c9bd21a01afa7f7ba2e7c82fda01090
Dishant shinde <dishantshinde13@gmail.com>
2024-07-10 20:58:13 +0530

    made changes to monogouri

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const userRouters = require("./routes/userRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+const mongouser = process.env.MONGO_USER;
+const mongopw = process.env.MONGO_PW;
 mongoose
   .connect(
-    "mongodb+srv://dishantshinde13:r0mJJJNhfJ9z9mOF@cluster0.imh2xjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    `mongodb+srv://${mongouser}:${mongopw}@cluster0.imh2xjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
