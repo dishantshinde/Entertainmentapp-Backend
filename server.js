a4378a2494699c62c9de5c0591fd0aaa1feb859f
Dishant shinde <dishantshinde13@gmail.com>
2024-07-13 14:18:04 +0530

    deletion of get static files and build

--- server.js
+++ server.js
@@ -25,16 +25,6 @@ mongoose
   .then(() => console.log("db connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
-
-// API routes
 app.use("/api/user", userRouters);
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
